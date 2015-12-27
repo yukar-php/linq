2f1401a133282749f21ae77bf0221d9d68800941
h-sugawara <nnfxt540@gmail.com>
2015-12-27 22:26:50 +0900

    To implement search processing methods.

---
+++ src/Enumerable/TSearch.php
@@ -0,0 +1,120 @@
+<?php
+namespace Yukar\Linq\Enumerable;
+
+/**
+ * シーケンスの検索に関する機能を提供します。
+ */
+trait TSearch
+{
+    use TQuery { where as private; }
+
+    /**
+     * シーケンスのすべての要素が条件を満たしているかどうかを判断します。
+     *
+     * @param \ArrayObject $source 述語を適用する要素を格納している ArrayObject
+     * @param \Closure $predicate 各要素が条件を満たしているかどうかをテストする関数
+     *
+     * @return bool シーケンスのすべての要素がテストに合格する場合は true。それ以外の場合は false。
+     */
+    public function all(\ArrayObject $source, \Closure $predicate): bool
+    {
+        return $this->where($source, $predicate)->count() === $source->count();
+    }
+
+    /**
+     * シーケンスの任意の要素が条件を満たしているかどうかを判断します。
+     *
+     * @param \ArrayObject $source 述語を適用する要素を格納している ArrayObject
+     * @param \Closure $predicate 各要素が条件を満たしているかどうかをテストする関数
+     *
+     * @return bool シーケンスの要素がテストに合格する場合は true。それ以外の場合は false。
+     */
+    public function any(\ArrayObject $source, \Closure $predicate): bool
+    {
+        return ($this->where($source, $predicate)->count() > 0);
+    }
+
+    /**
+     * 指定した要素がシーケンスに含まれているかどうかを判断します。
+     *
+     * @param \ArrayObject $source 値の検索対象となるシーケンス
+     * @param mixed $value シーケンス内で検索する値
+     *
+     * @return bool 指定した値を持つ要素がシーケンスに含まれている場合は true。それ以外は false。
+     */
+    public function contains(\ArrayObject $source, $value): bool
+    {
+        return in_array($value, $source->getArrayCopy());
+    }
+
+    /**
+     * シーケンス内の指定されたインデックス位置にある要素を返します。
+     *
+     * @param \ArrayObject $source 返される要素が含まれるシーケンス
+     * @param int $index 取得する要素の 0 から始まるインデックス
+     *
+     * @return mixed シーケンス内の指定された位置にある要素
+     */
+    public function elementAt(\ArrayObject $source, int $index)
+    {
+        if ($index < 0 || $index >= $source->count()) {
+            throw new \OutOfRangeException();
+        }
+
+        return $source->offsetGet($index);
+    }
+
+    /**
+     * 指定された条件を満たすシーケンスの最初の要素を返します。
+     *
+     * @param \ArrayObject $source 返される要素が含まれるシーケンス
+     * @param \Closure|null $predicate 各要素が条件を満たしているかどうかをテストする関数
+     *
+     * @return mixed 指定された述語関数でテストに合格するシーケンスの最初の要素
+     */
+    public function first(\ArrayObject $source, \Closure $predicate = null)
+    {
+        $target = $this->getArrayObject($source, $predicate)->getArrayCopy();
+
+        return array_shift($target);
+    }
+
+    /**
+     * 指定された条件を満たすシーケンスの最後の要素を返します。
+     *
+     * @param \ArrayObject $source 返される要素が含まれるシーケンス
+     * @param \Closure|null $predicate 各要素が条件を満たしているかどうかをテストする関数
+     *
+     * @return mixed 指定された述語関数でテストに合格するシーケンスの最後の要素
+     */
+    public function last(\ArrayObject $source, \Closure $predicate = null)
+    {
+        $target = $this->getArrayObject($source, $predicate)->getArrayCopy();
+
+        return array_pop($target);
+    }
+
+    /**
+     * 条件を満たす、シーケンスの唯一の要素を返します。そのような要素が複数存在する場合は、例外をスローします。
+     *
+     * @param \ArrayObject $source 1 つの要素を返すシーケンス
+     * @param \Closure $predicate 要素が条件を満たしているかどうかをテストする関数
+     *
+     * @return mixed 条件を満たす入力シーケンスの 1 つの要素
+     */
+    public function single(\ArrayObject $source, \Closure $predicate)
+    {
+        $result = $this->where($source, $predicate);
+
+        if ($result->count() !== 1) {
+            throw new \LogicException();
+        }
+
+        return $result[0];
+    }
+
+    private function getArrayObject(\ArrayObject $source, \Closure $predicate = null)
+    {
+        return ($predicate instanceof \Closure) ? $this->where($source, $predicate) : $source;
+    }
+}
---
+++ tests/src/Enumerable/TSearchTest.php
@@ -0,0 +1,249 @@
+<?php
+namespace Yukar\Linq\Tests\Enumerable;
+
+class TSearchTest extends \PHPUnit_Framework_TestCase
+{
+    public function providerAll()
+    {
+        return [
+            // 条件式True結果の動作確認
+            [ [ 1, 2, 3 ], true, function () { return true; } ],
+            // 条件式False結果の動作確認
+            [ [ 1, 2, 3 ], false, function () { return false; } ],
+            // スカラー型数値の動作確認
+            [ [ 1, 2, 3 ], false, function ($v) { return ($v % 2 === 0); } ],
+            // 文字列型の動作確認
+            [ [ 'a', 'a', 'a' ], true, function ($v) { return ($v === 'a'); } ],
+            // 論理型の動作確認
+            [ [ true, false, true ], false, function ($v) { return ($v === true); } ],
+            // 型混合配列（数値・論理型・オブジェクト型・配列）の動作確認
+            [ [ 0, true, new \stdClass(), [ 1 ] ], true, function ($v) { return isset($v); } ],
+            // 型混合配列（文字列・小数点数・クロージャ・NULL）の動作確認
+            [ [ 'a', 2.1, function() {}, null ], false, function ($v) { return isset($v); } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerAll
+     */
+    public function testAll($param, $expected, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('all')->will($this->returnValue($expected));
+
+        $result = $mock->all(new \ArrayObject($param), $predicate);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerAny()
+    {
+        return [
+            // 条件式True結果の動作確認
+            [ [ 1, 2, 3 ], true, function () { return true; } ],
+            // 条件式False結果の動作確認
+            [ [ 1, 2, 3 ], false, function () { return false; } ],
+            // スカラー型数値の動作確認
+            [ [ 1, 2, 3 ], true, function ($v) { return ($v % 2 === 0); } ],
+            // 文字列型の動作確認
+            [ [ 'a', 'a', 'a' ], false, function ($v) { return ($v === 'b'); } ],
+            // 論理型の動作確認
+            [ [ true, false, true ], true, function ($v) { return ($v === true); } ],
+            // 型混合配列（数値・論理型・オブジェクト型・配列）の動作確認
+            [ [ 0, true, new \stdClass(), [ 1 ] ], false, function ($v) { return isset($v) === false; } ],
+            // 型混合配列（文字列・小数点数・クロージャ・NULL）の動作確認
+            [ [ 'a', 2.1, function() {}, null ], true, function ($v) { return isset($v); } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerAny
+     */
+    public function testAny($param, $expected, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('any')->will($this->returnValue($expected));
+
+        $result = $mock->any(new \ArrayObject($param), $predicate);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerContains()
+    {
+        return [
+            // スカラー型数値
+            [ [ 1, 2, 3 ], true, 1 ],
+            // 文字列型
+            [ [ 'a', 'b', 'c' ], false, 'd' ],
+            // 論理型
+            [ [ true, false, true ], true, true ],
+            // オブジェクト型・クロージャ・NULL
+            [ [ new \stdClass(), function() {}, null ], false, [ 1, 2, 3 ] ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerContains
+     */
+    public function testContains($param, $expected, $value)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('contains')->will($this->returnValue($expected));
+
+        $result = $mock->contains(new \ArrayObject($param), $value);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerElementAt()
+    {
+        return [
+            [ [ 1, 2, 3 ], 2, 1 ],
+            [ [ 'a', 'b', 'c' ], 'c', 2 ],
+            [ [ true, false ], true, 0 ],
+            [ [ new \stdClass(), null ], null, 1 ],
+            [ [ 1.2, [ 2 ], function () {} ], [ 2 ], 1 ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerElementAt
+     */
+    public function testElementAt($param, $expected, $index)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('elementAt')->will($this->returnValue($expected));
+
+        $result = $mock->elementAt(new \ArrayObject($param), $index);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerElementAtErrors()
+    {
+        return [
+            // 範囲外（-1以下）
+            [ '\OutOfRangeException', [ 1, 2, 3 ], -1 ],
+            // 範囲外（長さ以上）
+            [ '\OutOfRangeException', [ 'a', 'b', 'c' ], 3 ],
+            // NULL
+            [ '\InvalidArgumentException', null, 0 ],
+            // 数値
+            [ '\InvalidArgumentException', 1, 0 ],
+            // 文字列型
+            [ '\InvalidArgumentException', 'abc', 0 ],
+            // 論理型
+            [ '\InvalidArgumentException', true, 0 ],
+            // 空配列
+            [ '\OutOfRangeException', [], 0 ],
+            // オブジェクト
+            [ '\OutOfRangeException', new \stdClass(), 0 ],
+            // クロージャ
+            [ '\OutOfRangeException', function () {}, 0 ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerElementAtErrors
+     */
+    public function testElementAtErrors($expectedException, $param, $index)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('elementAt');
+
+        $this->setExpectedException($expectedException);
+        $mock->elementAt(new \ArrayObject($param), $index);
+    }
+
+    public function providerFirst()
+    {
+        return [
+            [ [ 1, 2, 3 ], 2, function ($v) { return ($v % 2 === 0); } ],
+            [ [ 2, 3, 4 ], 3, function ($v) { return ($v % 2 !== 0); } ],
+            [ [ true, new \stdClass(), [] ], true, function ($v) { return is_bool($v) === true; } ],
+            [ [ 'abc', function () {}, null ], null, function ($v) { return isset($v) === false; } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerFirst
+     */
+    public function testFirst($param, $expected, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('first')->will($this->returnValue($expected));
+
+        $result = $mock->first(new \ArrayObject($param), $predicate);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerLast()
+    {
+        return [
+            [ [ 1, 2, 3 ], 3, function ($v) { return ($v % 2 !== 0); } ],
+            [ [ 2, 3, 4 ], 4, function ($v) { return ($v % 2 === 0); } ],
+            [ [ new \stdClass(), true, [] ], true, function ($v) { return is_array($v) === false; } ],
+            [ [ function () {}, 'abc', null ], 'abc', function ($v) { return isset($v) === true; } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerLast
+     */
+    public function testLast($param, $expected, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('last')->will($this->returnValue($expected));
+
+        $result = $mock->last(new \ArrayObject($param), $predicate);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerSingle()
+    {
+        return [
+            [ [ 1, 2, 3 ], 2, function ($v) { return ($v % 2 === 0); } ],
+            [ [ 2, 3, 4 ], 3, function ($v) { return ($v % 2 !== 0); } ],
+            [ [ true, new \stdClass(), [] ], true, function ($v) { return is_bool($v) === true; } ],
+            [ [ 'abc', function () {}, null ], null, function ($v) { return isset($v) === false; } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerSingle
+     */
+    public function testSingle($param, $expected, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('single')->will($this->returnValue($expected));
+
+        $result = $mock->single(new \ArrayObject($param), $predicate);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function providerSingleErrors()
+    {
+        return [
+            [ '\LogicException', [ 1, 2, 3 ], function ($v) { return ($v % 2 !== 0); } ],
+            [ '\LogicException', [ 2, 3, 4 ], function ($v) { return ($v % 2 === 0); } ],
+            [ '\LogicException', [ true, new \stdClass(), [] ], function ($v) { return is_bool($v) === false; } ],
+            [ '\LogicException', [ 'abc', function () {}, null ], function ($v) { return isset($v) === true; } ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerSingleErrors
+     */
+    public function testSingleErrors($expectedException, $param, $predicate)
+    {
+        $mock = $this->getMockForTrait('Yukar\Linq\Enumerable\TSearch');
+        $mock->expects($this->any())->method('single');
+
+        $this->setExpectedException($expectedException);
+        $mock->single(new \ArrayObject($param), $predicate);
+    }
+}
