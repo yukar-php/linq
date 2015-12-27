c02bd9cf7aa99986dc2d189fb0afe8a93d35dfc8
h-sugawara <nnfxt540@gmail.com>
2015-12-27 22:21:32 +0900

    Add comments to all methods on query trait.

--- src/Enumerable/TQuery.php
+++ src/Enumerable/TQuery.php
@@ -2,17 +2,18 @@
 namespace Yukar\Linq\Enumerable;
 
 /**
- * 
+ * 射影や選択を行う機能を提供します。
  */
 trait TQuery
 {
     /**
+     * 要素のインデックスを組み込むことにより、シーケンスの各要素を新しいフォームに射影します。
      * 
-     * 
-     * @param \ArrayObject $source
-     * @param \Closure $selector
+     * @param \ArrayObject $source 変換関数を呼び出す対象となる値のシーケンス
+     * @param \Closure $selector 各ソース要素に適用する変換関数。
+     *                           この関数の 2 つ目のパラメーターは、ソース要素のインデックスを表します。
      *
-     * @return \ArrayObject
+     * @return \ArrayObject 各要素に対して変換関数を呼び出した結果として得られる要素を含むシーケンス
      */
     public function select(\ArrayObject $source, \Closure $selector): \ArrayObject
     {
@@ -26,11 +27,11 @@ trait TQuery
     }
 
     /**
+     * シーケンスから一意の要素を返します。
      * 
-     * 
-     * @param \ArrayObject $source
+     * @param \ArrayObject $source 重複する要素を削除する対象となるシーケンス
      *
-     * @return \ArrayObject
+     * @return \ArrayObject ソースとなるシーケンスの一意の要素を格納するシーケンス
      */
     public function distinct(\ArrayObject $source): \ArrayObject
     {
@@ -38,12 +39,14 @@ trait TQuery
     }
     
     /**
+     * 述語に基づいて値のシーケンスをフィルター処理します。
      * 
-     * 
-     * @param \ArrayObject $source
-     * @param \Closure $predicate
+     * @param \ArrayObject $source フィルター処理するシーケンス
+     * @param \Closure $predicate 各ソース要素が条件に当てはまるかどうかをテストする関数。
+     *                            この関数の 2 つ目のパラメーターは、ソース要素のインデックスを表します。
      *
-     * @return \ArrayObject
+     * @return \ArrayObject 各ソース要素が条件に当てはまるかどうかをテストする関数。
+     * この関数の 2 つ目のパラメーターは、ソース要素のインデックスを表します。
      */
     public function where(\ArrayObject $source, \Closure $predicate): \ArrayObject
     {
