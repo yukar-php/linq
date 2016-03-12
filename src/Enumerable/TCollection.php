e70032dfe6a1f268e92aa3691f3ccd17f517a066
h-sugawara <nnfxt540@gmail.com>
2016-03-12 13:42:44 +0900

    Deleted some methods from TCollection Trait and Changed all of the methods belonging to it to the statically method.

--- src/Enumerable/TCollection.php
+++ src/Enumerable/TCollection.php
@@ -2,7 +2,7 @@
 namespace Yukar\Linq\Enumerable;
 
 /**
- * シーケンスの要素に関する処理を提供します。
+ * シーケンスの要素の生成に関する処理を提供します。
  */
 trait TCollection
 {
@@ -11,21 +11,9 @@ trait TCollection
      *
      * @return \ArrayObject 空のシーケンス
      */
-    public function emptyList(): \ArrayObject
+    public static function emptyList(): \ArrayObject
     {
         return new \ArrayObject([]);
-    }
-
-    /**
-     * シーケンスのコピーを返します。
-     *
-     * @param \ArrayObject $source コピー元のシーケンス
-     *
-     * @return \ArrayObject 入力シーケンスのコピーとなるシーケンス
-     */
-    public function asEnumerable(\ArrayObject $source): \ArrayObject
-    {
-        return new \ArrayObject($source->getArrayCopy());
     }
 
     /**
@@ -36,7 +24,7 @@ trait TCollection
      *
      * @return \ArrayObject 連続した整数の範囲を含むシーケンス
      */
-    public function range(int $start, int $count): \ArrayObject
+    public static function range(int $start, int $count): \ArrayObject
     {
         if ($count < 0) {
             throw new \OutOfRangeException();
@@ -53,51 +41,8 @@ trait TCollection
      *
      * @return \ArrayObject 繰り返される値を含むシーケンス
      */
-    public function repeat($element, int $count): \ArrayObject
+    public static function repeat($element, int $count): \ArrayObject
     {
         return new \ArrayObject(array_fill(0, $count, $element));
-    }
-
-    /**
-     * シーケンスの要素を、指定した型にキャストします。
-     *
-     * @param \ArrayObject $source 型 $type にキャストされる要素が格納されているシーケンス
-     * @param string $type キャストする型の名前
-     *
-     * @return \ArrayObject 入力シーケンスの各要素を指定された型にキャストした要素を格納するシーケンス
-     */
-    public function cast(\ArrayObject $source, string $type): \ArrayObject
-    {
-        $dst = $this->emptyList();
-
-        foreach ($source->getIterator() as $key => $value) {
-            if (is_scalar($value) === false) {
-                throw new \LogicException();
-            }
-
-            $dst->append(call_user_func(strtolower($type) . "val", $value));
-        }
-
-        return $dst;
-    }
-
-    /**
-     * 指定された型に基づいてシーケンスの要素をフィルター処理します。
-     *
-     * @param \ArrayObject $source フィルター処理する要素を含むシーケンス
-     * @param string $type シーケンスの要素をフィルター処理する型の名前
-     *
-     * @return \ArrayObject 入力シーケンスの各要素を指定された型にキャストした要素を格納するシーケンス。
-     *                      キャストに失敗した要素は含まれません。
-     */
-    public function ofType(\ArrayObject $source, string $type): \ArrayObject
-    {
-        $dst = $this->emptyList();
-
-        foreach ($source->getIterator() as $key => $value) {
-            (is_scalar($value) === true) && $dst->append(call_user_func(strtolower($type) . "val", $value));
-        }
-
-        return $dst;
     }
 }
