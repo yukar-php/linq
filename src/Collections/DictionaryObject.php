6c0dce14fab1218ea388f0816dfd3d3465610d3e
h-sugawara <nnfxt540@gmail.com>
2016-06-13 21:45:23 +0900

    Changed DictionaryObject class.

--- src/Collections/DictionaryObject.php
+++ src/Collections/DictionaryObject.php
@@ -1,140 +1,269 @@
 <?php
 namespace Yukar\Linq\Collections;
 
+use Traversable;
+use Yukar\Linq\Interfaces\Collections\IDictionary;
+
 /**
- * オブジェクトをハッシュテーブルとして動作させます。
+ * キーと値のペアを持つコレクションを表します。
  */
-class DictionaryObject extends \ArrayObject
+class DictionaryObject extends BaseCommonCollection implements IDictionary
 {
     private $key_list = [];
     private $key_type = null;
     private $value_type = null;
 
     /**
-     * DictionaryObject クラスの新しいインスタンスを初期化します。
+     * BaseDictionary クラスの新しいインスタンスを初期化します。
      *
-     * @param int $flags
-     * @param string $iterator_class
+     * @param array $input 初期化時に保持する要素のリスト
      */
-    public function __construct(int $flags = 0, string $iterator_class = 'ArrayIterator')
+    public function __construct(array $input = [])
     {
-        parent::__construct([], $flags, $iterator_class);
+        parent::__construct([]);
+        (empty($input) === false) && $this->addRange($input);
     }
 
     /**
-     * シーケンスに値を追加します。
+     * Retrieve an external iterator
      *
-     * @param KeyValuePair $value キーと値のペア
+     * @link  http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     *        <b>Traversable</b>
+     * @since 5.0.0
      */
-    public function append($value)
+    public function getIterator()
     {
-        if ($value instanceof KeyValuePair === false) {
+        return new DictionaryIterator($this->getSourceList()->getArrayCopy());
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return $this->containsKey($offset);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->getSourceList()->offsetGet($this->getKeyOffset($offset))->getValue();
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     *                      </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     *                      </p>
+     *
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->add($offset, $value);
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     *
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        $this->remove($offset);
+    }
+
+    /**
+     * 指定したキーと値のペアの要素をコレクションに追加します。
+     *
+     * @param mixed $key   追加する要素のキー
+     * @param mixed $value 追加する要素の値
+     */
+    public function add($key, $value)
+    {
+        if ($this->setAllowTypes($key, $value) === false) {
             throw new \InvalidArgumentException();
         }
 
-        $this->offsetSet($value->getKey(), $value->getValue());
+        $this->getSourceList()->offsetSet($this->setKey($key), new KeyValuePair($key, $value));
     }
 
     /**
-     * シーケンスに指定したキーで値を追加します。
+     * 指定したキーの要素がコレクションに存在するかどうかを調べます。
      *
-     * @param mixed $key シーケンスに追加するキー
-     * @param mixed $value キーに紐づけられる値
+     * @param mixed $key コレクション内で検索されるキー
+     *
+     * @return bool 指定したキーの要素がコレクションに存在する場合は true。それ以外の場合は false。
      */
-    public function offsetSet($key, $value)
+    public function containsKey($key): bool
     {
-        $this->addKeyList($key);
-        ($this->isAllowableTypes($key, $value) === true) && parent::offsetSet($this->count(), new KeyValuePair($key, $value));
+        return ($this->getKeyOffset($key) > -1);
     }
 
     /**
-     * シーケンスから指定したキーから値を取得します。
+     * コレクションに特定の値が格納されているかどうかを調べます。
      *
-     * @param mixed $key 値を取得するキー
+     * @param mixed $value コレクションから探す値
      *
-     * @return mixed シーケンスのキーから取得した値
+     * @return bool コレクションに指定した値が存在する場合は true。それ以外の場合は false。
      */
-    public function offsetGet($key)
+    public function containsValue($value): bool
     {
-        if ($this->offsetExists($key) === false) {
-            throw new \OutOfBoundsException();
+        return in_array($value, $this->getValues(), true);
+    }
+
+    /**
+     * コレクションが保持しているキーの一覧を取得します。
+     *
+     * @return array コレクションが保持しているキーの一覧
+     */
+    public function getKeys(): array
+    {
+        return array_keys($this->key_list);
+    }
+
+    /**
+     * コレクションの保持している値の一覧を取得します。
+     *
+     * @return array コレクションの保持している値の一覧
+     */
+    public function getValues(): array
+    {
+        $values = [];
+
+        /** @var KeyValuePair $value */
+        foreach ($this->getSourceList()->getArrayCopy() as $value) {
+            $values[] = $value->getValue();
         }
 
-        return parent::offsetGet($this->getKeyListValue($key));
+        return $values;
     }
 
     /**
-     * シーケンスに指定したキーに値が存在するかどうかを判別します。
+     * 指定したキーを持つ要素をコレクションから削除します。
      *
-     * @param mixed $key 値が存在するかどうかを判別するキー
+     * @param mixed $key コレクションから削除する要素のキー
      *
-     * @return bool シーケンスに指定した値が存在する場合は true。それ以外の場合は false。
+     * @return bool 要素がコレクションから正常に削除された場合は true。それ以外の場合は false。
      */
-    public function offsetExists($key): bool
+    public function remove($key): bool
     {
-        return parent::offsetExists($this->getKeyListValue($key));
-    }
-
-    /**
-     * シーケンスから指定したキーとその値を削除します。
-     *
-     * @param mixed $key シーケンスから削除するキー
-     */
-    public function offsetUnset($key)
-    {
-        parent::offsetUnset($this->getKeyListValue($key));
-        $this->unsetKeyListValue($key);
-    }
-
-    /**
-     * シーケンスの内容を連想配列として取得します。
-     *
-     * @return array シーケンスの連想配列
-     */
-    public function getDictionaryCopy()
-    {
-        $hash_list = [];
-
-        foreach ($this->getIterator() as $value) {
-            $hash_list[$value->getKey()] = $value->getValue();
-        }
-
-        return $hash_list;
-    }
-
-    private function addKeyList($key)
-    {
-        $this->key_list[$key] = $this->count();
-    }
-
-    private function getKeyListValue($key)
-    {
-        return array_key_exists($key, $this->key_list) ? $this->key_list[$key] : $key;
-    }
-
-    private function unsetKeyListValue($key)
-    {
-        unset($this->key_list[$key]);
-    }
-
-    private function setDictionaryTypes($key, $value)
-    {
-        if ($this->count() === 0) {
-            $this->key_type = gettype($key);
-            $this->value_type = gettype($value);
+        if ($this->containsKey($key) === true) {
+            $this->getSourceList()->offsetUnset($this->setKey($key));
+            $this->removeKey($key);
+            $this->getSourceList()->exchangeArray(array_values($this->getSourceList()->getArrayCopy()));
 
             return true;
         }
 
-        return ($this->key_type === gettype($key) && $this->value_type === gettype($value));
+        return false;
     }
 
-    private function isAllowableTypes($key, $value)
+    /**
+     * 指定したキーに関連付けされている値を取得します。
+     *
+     * @param mixed $key   値を取得するキー
+     * @param mixed $value キーが存在する場合はその値を、それ以外の時はNULLを代入する
+     *
+     * @return bool 指定したキーを持つ要素がコレクションに存在する場合は true。それ以外の場合は false。
+     */
+    public function tryGetValue($key, &$value): bool
     {
-        if ($this->setDictionaryTypes($key, $value) === false) {
-            throw new \UnexpectedValueException();
+        $is_exist_offset = $this->containsKey($key);
+
+        ($is_exist_offset === true) && $value = $this->offsetGet($key);
+
+        return $is_exist_offset;
+    }
+
+    protected function setKey($key): int
+    {
+        if ($this->getKeyOffset($key) < 0) {
+            $this->key_list[$key] = $this->getSourceList()->count();
         }
 
-        return true;
+        return $this->getKeyOffset($key);
+    }
+
+    protected function getKeyOffset($key): int
+    {
+        return $this->key_list[$key] ?? -1;
+    }
+
+    protected function removeKey($key)
+    {
+        if ($this->getKeyOffset($key) > -1) {
+            unset($this->key_list[$key]);
+
+            if (count($this->key_list) > 0) {
+                $this->key_list = array_combine(
+                    array_keys($this->key_list),
+                    range(0, count($this->key_list) - 1)
+                );
+            }
+        }
+    }
+
+    protected function setAllowTypes($key, $value): bool
+    {
+        $target_key_type = gettype($key);
+        $target_value_type = gettype($value);
+
+        if (count($this->key_list) === 0) {
+            $this->key_type = $target_key_type;
+            $this->value_type = $target_value_type;
+
+            return true;
+        }
+
+        return ($this->key_type === $target_key_type && $this->value_type === $target_value_type);
+    }
+
+    protected function addRange(array $target_list)
+    {
+        $keys = array_keys($target_list);
+        $values = array_values($target_list);
+
+        for ($i = 0, $length = count($target_list); $i < $length; $i++) {
+            $this->add(array_shift($keys), array_shift($values));
+        }
     }
 }
--- tests/src/Collections/DictionaryObjectTest.php
+++ tests/src/Collections/DictionaryObjectTest.php
@@ -6,75 +6,195 @@ use Yukar\Linq\Collections\KeyValuePair;
 
 class DictionaryObjectTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAppend()
+    public function testGetIterator()
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->append(new KeyValuePair("key", "value"));
+        $base_list = [ 'str1' => 1 ];
+        $target_key = 'str1';
 
-        $result = $dic_obj->getArrayCopy();
+        $object = new DictionaryObject($base_list);
 
-        $this->assertCount(1, $result);
-        $this->assertInstanceOf('Yukar\Linq\Collections\KeyValuePair', $result[0]);
+        $iterator = $object->getIterator();
+        $iterator[$target_key] = 2;
+
+        $this->assertNotSame($object[$target_key], $iterator[$target_key]);
+        $this->assertSame($base_list[$target_key], $object[$target_key]);
     }
 
-    public function testOffsetSet()
+    public function testArrayAccess()
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->offsetSet("key", "value");
+        $object = new DictionaryObject();
+        $object['first'] = 1;
 
-        $result = $dic_obj->getArrayCopy();
+        $method_reflector = (new \ReflectionClass($object))->getMethod('getSourceList');
+        $method_reflector->setAccessible(true);
 
-        $this->assertCount(1, $result);
-        $this->assertInstanceOf('Yukar\Linq\Collections\KeyValuePair', $result[0]);
+        $this->assertSame($method_reflector->invoke($object)->getArrayCopy()[0]->getValue(), $object['first']);
+
+        unset($object['first']);
+
+        $this->assertFalse(isset($object['first']));
     }
 
-    public function testOffsetGet()
+    public function providerAdd()
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->append(new KeyValuePair("key", "value"));
+        $object_1 = new \stdClass();
+        $object_2 = new \stdClass();
+        $closure_1 = function () {
+            return true;
+        };
+        $closure_2 = function () {
+            return false;
+        };
 
-        $result = $dic_obj->offsetGet("key");
-
-        $this->assertEquals("value", $result);
+        return [
+            // キーが整数値型、値が文字列型、初期化時リストなし
+            [ [ new KeyValuePair(1, 'first') ], [], 1, 'first' ],
+            // キーが整数値型、値が文字列型、初期化時リストあり
+            [ [ new KeyValuePair(0, 'based'), new KeyValuePair(1, 'first') ], [ 'based' ], 1, 'first' ],
+            // キーが文字列型、値が数値型、初期化時リストなし
+            [ [ new KeyValuePair('first', 1) ], [], 'first', 1 ],
+            // キーが文字列型、値が数値型、初期化時リストあり
+            [ [ new KeyValuePair('based', 0), new KeyValuePair('first', 1) ], [ 'based' => 0 ], 'first', 1 ],
+            // キーが整数値型、値がオブジェクト型、初期化時リストなし
+            [ [ new KeyValuePair(1, $object_2) ], [], 1, $object_2 ],
+            // キーが整数値型、値がオブジェクト型、初期化時リストあり
+            [ [ new KeyValuePair(0, $object_1), new KeyValuePair(1, $object_2) ], [ $object_1 ], 1, $object_2 ],
+            // キーが文字列型、値がクロージャ型、初期化時リストなし
+            [ [ new KeyValuePair('first', $closure_2) ], [], 'first', $closure_2 ],
+            // キーが文字列型、値がクロージャ型、初期化時リストあり
+            [ [ new KeyValuePair('based', $closure_1), new KeyValuePair('first', $closure_2) ], [ 'based' => $closure_1 ], 'first', $closure_2 ],
+        ];
     }
 
-    public function testOffsetExists()
+    /**
+     * @dataProvider providerAdd
+     */
+    public function testAdd($expected, $base_list, $key, $value)
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->offsetSet("key", "value");
+        $object = new DictionaryObject($base_list);
+        $object->add($key, $value);
 
-        $this->assertTrue($dic_obj->offsetExists("key"));
-        $this->assertFalse($dic_obj->offsetExists("key2"));
+        $method_reflector = (new \ReflectionClass($object))->getMethod('getSourceList');
+        $method_reflector->setAccessible(true);
+
+        $this->assertEquals($expected, $method_reflector->invoke($object)->getArrayCopy());
     }
 
-    public function testOffsetUnset()
+    public function providerAddFailure()
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->append(new KeyValuePair("key", "value"));
-        $dic_obj->offsetUnset("key");
-
-        $this->setExpectedException('\OutOfBoundsException');
-        $dic_obj->offsetGet("key");
+        return [
+            // 追加時にエラー
+            [ '\InvalidArgumentException', [ 1 => 'str' ], 'str2', 2 ],
+            // 初期化時にエラー
+            [ '\InvalidArgumentException', [ 1 => 'str', 'str2' => 2 ], 3, 'str3' ],
+        ];
     }
 
-    public function testTwiceAppend()
+    /**
+     * @dataProvider providerAddFailure
+     */
+    public function testAddFailure($expected, $base_list, $key, $value)
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->append(new KeyValuePair("key", "value"));
-        $dic_obj->offsetSet("key2", "value2");
-
-        $result = $dic_obj->getDictionaryCopy();
-
-        $this->assertCount(2, $result);
-        $this->assertEquals([ "key" => "value", "key2" => "value2" ], $result);
+        $this->expectException($expected);
+        (new DictionaryObject($base_list))->add($key, $value);
     }
 
-    public function testTwiceAppendFailed()
+    public function providerContainsKey()
     {
-        $dic_obj = new DictionaryObject();
-        $dic_obj->offsetSet(1, "value2");
+        return [
+            [ true, [ 'str1' => 1, 'str2' => 2 ], 'str1' ],
+            [ false, [ 'str1' => 1, 'str2' => 2 ], 'str3' ],
+        ];
+    }
 
-        $this->setExpectedException('\UnexpectedValueException');
-        $dic_obj->append(new KeyValuePair("key", "value"));
+    /**
+     * @dataProvider providerContainsKey
+     */
+    public function testContainsKey($expected, $base_list, $key)
+    {
+        $this->assertSame($expected, (new DictionaryObject($base_list))->containsKey($key));
+    }
+
+    public function providerContainsValue()
+    {
+        return [
+            [ true, [ 'str1' => 1, 'str2' => 2 ], 2 ],
+            [ false, [ 'str1' => 1, 'str2' => 2 ], 3 ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerContainsValue
+     */
+    public function testContainsValue($expected, $base_list, $value)
+    {
+        $this->assertSame($expected, (new DictionaryObject($base_list))->containsValue($value));
+    }
+
+    public function testGetKeys()
+    {
+        $base_list = [ 'key1' => 1, 'key2' => 2, 'key3' => 3 ];
+        $expected = [ 'key1', 'key2', 'key3' ];
+
+        $this->assertSame($expected, (new DictionaryObject($base_list))->getKeys());
+    }
+
+    public function testGetValues()
+    {
+        $base_list = [ 'key1' => 1, 'key2' => 2, 'key3' => 3 ];
+        $expected = [ 1, 2, 3 ];
+
+        $this->assertSame($expected, (new DictionaryObject($base_list))->getValues());
+    }
+
+    public function providerRemove()
+    {
+        return [
+            [ [ true ], [ new KeyValuePair('str2', 2) ], [ 'str1' => 1, 'str2' => 2 ], [ 'str1' ] ],
+            [ [ false ], [ new KeyValuePair('str1', 1), new KeyValuePair('str2', 2) ], [ 'str1' => 1, 'str2' => 2 ], [ 'str3' ] ],
+            [ [ true, true ], [], [ 'str1' => 1, 'str2' => 2 ], [ 'str1', 'str2' ] ],
+            [ [ true, false ], [ new KeyValuePair('str2', 2) ], [ 'str1' => 1, 'str2' => 2 ], [ 'str1', 'str3' ] ],
+            [ [ false, true ], [ new KeyValuePair('str2', 2) ], [ 'str1' => 1, 'str2' => 2 ], [ 'str0', 'str1' ] ],
+            [ [ false, false ], [ new KeyValuePair('str1', 1), new KeyValuePair('str2', 2) ], [ 'str1' => 1, 'str2' => 2 ], [ 'str3', 'str4' ] ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerRemove
+     */
+    public function testRemove($expected, $after_list, $base_list, $keys)
+    {
+        $object = new DictionaryObject($base_list);
+
+        foreach ($keys as $index => $key) {
+            $this->assertSame($expected[$index], $object->remove($key));
+        }
+
+        $reflector = new \ReflectionClass($object);
+        $method_reflector = $reflector->getMethod('getSourceList');
+        $method_reflector->setAccessible(true);
+
+        $this->assertEquals($after_list, $method_reflector->invoke($object)->getArrayCopy());
+    }
+
+    public function providerTryGetValue()
+    {
+        return [
+            [ true, 1, [ 'str1' => 1 ], 'str1' ],
+            [ false, null, [ 'str1' => 1 ], 'str2' ],
+            [ true, 2, [ 'str1' => 1, 'str2' => 2 ], 'str2' ],
+            [ false, null, [ 'str1' => 1, 'str2' => 2 ], 'str3' ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerTryGetValue
+     */
+    public function testTryGetValue($expected, $expected_value, $base_list, $key)
+    {
+        $value = null;
+
+        $this->assertSame($expected, (new DictionaryObject($base_list))->tryGetValue($key, $value));
+        $this->assertSame($expected_value, $value);
     }
 }
